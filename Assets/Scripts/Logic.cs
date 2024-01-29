28c485390c51fab33f8b15c874e5aa63b3b645aa
jan <57322982+mvte@users.noreply.github.com>
2024-01-29 16:04:59 -0500

    logic script to handle game logic

---
+++ Assets/Scripts/Logic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic : MonoBehaviour
+{
+    public ShipManager ship;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ship.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
