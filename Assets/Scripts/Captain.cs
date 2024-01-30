05df155c513f77668b555c9b4cbb299724516b9d
jan <57322982+mvte@users.noreply.github.com>
2024-01-30 10:59:13 -0500

    no need for Move method

--- Assets/Scripts/Captain.cs
+++ Assets/Scripts/Captain.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Captain : MonoBehaviour
-{
-    public void Move(Vector2 pos) {
-        transform.position = new Vector3(pos.x, pos.y, 0);
-    }
+{   
+    public Vector2 pos;
 }
