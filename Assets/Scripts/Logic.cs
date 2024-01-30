fafdbe86c096bb24883df6a53365141b86c7f1a2
mvte <janmarzan11@gmail.com>
2024-01-29 22:24:46 -0500

    update camera ref position and size

--- Assets/Scripts/Logic.cs
+++ Assets/Scripts/Logic.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Logic : MonoBehaviour
 {
     public ShipManager ship;
+    public GameObject cam;
 
     // Start is called before the first frame update
     void Start()
     {
         ship.Init();
+        cam.transform.position = new Vector3(1, ship.dim/2 - 0.5f, -10);
+        cam.GetComponent<Camera>().orthographicSize = ship.dim * 9 / 16;
     }
 
     // Update is called once per frame
@@ -17,4 +21,7 @@ public class Logic : MonoBehaviour
     {
         
     }
+
+
+    
 }
