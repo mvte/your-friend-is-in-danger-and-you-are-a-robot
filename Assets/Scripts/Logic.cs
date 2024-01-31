900cffc08d20fdcaf19862bd66c4b18ba67c9afe
jan <57322982+mvte@users.noreply.github.com>
2024-01-30 21:35:26 -0500

    implement simulation logic

--- Assets/Scripts/Logic.cs
+++ Assets/Scripts/Logic.cs
@@ -8,7 +8,12 @@ public class Logic : MonoBehaviour
     public ShipManager ship;
     public Bot1 bot1Ref;
     public GameObject cam;
+    public bool running;
+    public int steps;
+    public int runs = 1;
 
+    public bool animate = false;
+    private float accumulatedTime = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,54 +21,76 @@ public class Logic : MonoBehaviour
         cam.transform.position = new Vector3(1, ship.dim/2 - 0.5f, -10);
         cam.GetComponent<Camera>().orthographicSize = ship.dim * 9 / 16;
 
+        // only for now
         RunSimulation();
-    }
+    } 
 
-    public void RunSimulation(int runs = 1, bool animate = false) {    
-        for(int i = 0; i < runs; i++) {
-            Debug.Log($"Simulation {i + 1} started");
-            ship.Reset();
-            ship.Ready();
-
-            // advance the bot
-            ship.bot.computeNextStep(ship);
-
-            // determine if the bot is on an alien
-            foreach(Alien alien in ship.aliens) {
-                if(ship.bot.pos == alien.pos) {
-                    Debug.Log("Bot hit alien");
-                    continue;
-                }
-            }
-            
-            // determine if the bot is on the captain
-            if(ship.bot.pos == ship.captain.pos) {
-                Debug.Log("Bot reached the captain");
-                continue;
-            }
-
-            // advance aliens
-            List<Alien> randomizedAliens = new List<Alien>(ship.aliens);
-            randomizedAliens.Shuffle();
-            foreach(Alien alien in randomizedAliens) {
-                alien.computeNextStep(ship);
-            }
-
-            // check if the bot is on an alien
-            foreach(Alien alien in ship.aliens) {
-                if(ship.bot.pos == alien.pos) {
-                    Debug.Log("Bot hit alien");
-                    continue;
-                }
-            }
-
-        }
+    //TODO: map to button
+    public void RunSimulation() {    
+        Debug.Log("Simulation started");
+        running = true;
+        ship.Reset();
+        ship.Ready();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        if(!running) {
+            return;
+        }
+        if(steps > 1000) {
+            steps = 0;
+            Debug.Log("simulation end");
+            runs--;
+            return;
+        }
+        if(runs == 0) {
+            running = false;
+            return;
+        }
+
+        if(animate) {
+            accumulatedTime += Time.deltaTime;
+            if(accumulatedTime < 0.2f) {
+                return;
+            }
+        }
+        accumulatedTime = 0;
+
+        // advance the bot
+        ship.bot.computeNextStep(ship);
+
+        // determine if the bot is on an alien
+        Node botNode = ship.GetNode(ship.bot.pos);
+        if(botNode.occupied) {
+            Debug.Log("Bot hit alien");
+            steps = 1001;
+            return;
+        }
         
+        // determine if the bot is on the captain
+        if(ship.bot.pos == ship.captain.pos) {
+            Debug.Log("Bot reached the captain");
+            steps = 1001;
+            return;
+        }
+
+        // advance aliens
+        List<Alien> randomizedAliens = new List<Alien>(ship.aliens);
+        randomizedAliens.Shuffle();
+        foreach(Alien alien in randomizedAliens) {
+            alien.computeNextStep(ship);
+        }
+
+        // check if the bot is on an alien
+        if(botNode.occupied) {
+            Debug.Log("Bot hit alien");
+            steps = 1001;
+            return;
+        }
+
+        steps++;
     }
     
 }
@@ -81,3 +108,5 @@ static class ListExtensions {
         }
     }
 }
+
+
