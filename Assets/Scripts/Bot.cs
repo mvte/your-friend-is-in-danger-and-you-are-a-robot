63d985e621726046c5a83682e42194e519132992
jan <57322982+mvte@users.noreply.github.com>
2024-01-30 21:34:31 -0500

    implement next move logic for bot1

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -6,5 +6,5 @@ public abstract class Bot: MonoBehaviour
     public Vector2 pos { get; set; }
 
     // computes the next step and moves the bot
-    public abstract Vector2 computeNextStep(ShipManager ship);
+    public abstract void computeNextStep(ShipManager ship);
 }
--- Assets/Scripts/Bot1.cs
+++ Assets/Scripts/Bot1.cs
@@ -1,29 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
+using Utils;
 using UnityEngine;
 
 public class Bot1 : Bot
 {
-    List<Vector2> path;
+    List<Node> path;
 
     /**
     * This bot uses A* to find the shortest path to the captain
     */
-    public override Vector2 computeNextStep(ShipManager ship) {
+    public override void computeNextStep(ShipManager ship) {
         if(path == null) {
-            path = a_star(ship.captain, ship.aliens, ship.nodes);
+            Node startNode = ship.GetNode(this.pos);
+            Node captainNode = ship.GetNode(ship.captain.pos);
+            path = a_star(startNode, captainNode, ship.aliens, ship.nodes, ship);
         }
         if(path.Count == 0) {
-            return pos;
+            return;
         }
 
-        Vector2 next = path[0];
+        Vector2 next = path[0].pos;
+        path[0].Highlight();
         path.RemoveAt(0);
-        return next;
+
+        this.pos = next;
+        this.transform.position = new Vector3(next.x, next.y, 0);
     }
 
-    private List<Vector2> a_star(Captain captain, List<Alien> aliens, Dictionary<Vector2, Node> nodes) {
-        //TODO: implement this
-        return new List<Vector2>();
+    private List<Node> a_star(Node s, Node g, List<Alien> aliens, Dictionary<Vector2, Node> nodes, ShipManager ship) {
+        var fringe = new PriorityQueue<Node, float>();
+        fringe.Enqueue(s, 0);
+        var prev = new Dictionary<Node, Node>();
+        prev[s] = s;
+        var dist = new Dictionary<Node, float>();
+        dist[s] = 0;
+
+        while(fringe.Count > 0) {
+            Node curr = fringe.Dequeue();
+            if(curr.pos == g.pos) {
+                break;
+            }
+            List<Node> neighbors = ship.GetValidNeighborNodes(curr.pos);
+            foreach(Node n in neighbors) {
+                float tempDist = dist[curr] + 1.0f;
+                if (!dist.ContainsKey(n) || tempDist < dist[n]) {
+                    dist[n] = tempDist;
+                    prev[n] = curr;
+                    fringe.Enqueue(n, tempDist + heuristic(n, g));
+                }
+            }
+        }
+
+        // there is no path to g
+        if(!prev.ContainsKey(g)) {
+            return new List<Node>();
+        }
+
+        // construct the path
+        List<Node> path = new List<Node>();
+        Node currentNode = g;
+        while(currentNode.pos != s.pos) {
+            path.Add(currentNode);
+            currentNode = prev[currentNode];
+        }
+        path.Reverse();
+
+        // highlight the path
+        foreach(Node v in path) {
+           v.Highlight();
+        }
+        return path;
+    }
+
+    // we use the manhattan distance as our heuristic
+    private float heuristic(Node n, Node g) {
+        return Mathf.Abs(n.pos.x - g.pos.x) + Mathf.Abs(n.pos.y - g.pos.y);
     }
 }
