c7ec47bb9d5cca1298f3a4f376dbaac29dbd6d49
mvte <janmarzan11@gmail.com>
2024-02-29 20:46:35 -0500

    compute k correctly

--- Report/Analysis/find_k.py
+++ Report/Analysis/find_k.py
@@ -6,19 +6,19 @@ import sys
 # python find_k.py 1
 X = 1 if not sys.argv else float(sys.argv[1])
 
-
+# must be true for all m > N, not just m = len(data)
 def compute_k(data):
-    i = len(data) - 1
-    count = 0
-    total = 0
-
-    while count == 0 or total/count <= X:
-        total += float(data[i][1])
-        count += 1
-        i -= 1
-
-    return i
-
+    for N in range(1, len(data) - 1):
+        for m in range(N + 1, len(data)):
+            total = sum(float(row[1]) for row in data[N + 1: m + 1])
+            average = total / (m - N)
+        
+            if average >= X:
+                break
+        else:
+            return N
+    
+    return None
 
 bot1Data = []
 with open('Bot1Data.csv') as file:
